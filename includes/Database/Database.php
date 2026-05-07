0b667591706f74fd279403c1b5884b0062211ac4
JadePulong <christianjadepulong6@gmail.com>
2026-05-07 23:55:28 +0800

    password

--- includes/Database/Database.php
+++ includes/Database/Database.php
@@ -4,12 +4,12 @@
             private string $host,
             private string $name,
             private string $user,
-            private string $password = ""
+            private string $password
         ){}
 
         public function connectDatabase(): PDO {
             $dsn = "mysql:$this->host;dbname=$this->name;charset=utf8";
-            $pdo = new PDO($dsn, $this->user, $this->host, [
+            $pdo = new PDO($dsn, $this->user, $this->password, [
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             ]);
