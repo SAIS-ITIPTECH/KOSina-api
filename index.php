12aaadcd92b688cb09df6a258b8f2d6d7ce3d226
JadePulong <christianjadepulong6@gmail.com>
2026-05-08 01:20:02 +0800

    response

--- index.php
+++ index.php
@@ -82,6 +82,7 @@ class Main{
             return null;
 
         } else if ($this->table == "confirm") {
+            http_response_code(200);
             $confirmPaid = new ConfirmPaid();
             $confirmPaid->checkPaid();
             return null;
