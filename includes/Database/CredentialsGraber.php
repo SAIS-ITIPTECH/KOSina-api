a8465cca073a2734f9f21a7546749cee7d93409e
JadePulong <christianjadepulong6@gmail.com>
2026-05-08 02:17:34 +0800

    j

--- includes/Database/CredentialsGraber.php
+++ includes/Database/CredentialsGraber.php
@@ -20,11 +20,15 @@ class CredentialsGraber{
     }
 
     public function connectCredentials($target){
+        error_log("--- NEW 3 WEBHOOK ---");
+        error_log(json_encode($target));
         $query = "SELECT db_name, db_username, db_password FROM accounts WHERE $target = :setid";
         $stmt = $this->pdo->prepare($query);
         $stmt->bindValue(":setid", "$this->id");
         $this->execution->execute($stmt);
         $result = $this->execution->getResults();
+        error_log("--- NEW 4 WEBHOOK ---");
+        error_log(json_encode($result));
         $this->dbName = $result[0]["db_name"] ?? null;
         $this->dbUsername = $result[0]["db_username"] ?? null;
         $this->dbPassword = $result[0]["db_password"] ?? null;
