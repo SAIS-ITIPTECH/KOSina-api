a2529baa902895472af8272d0efaaebd30436a81
JadePulong <christianjadepulong6@gmail.com>
2026-05-08 00:42:27 +0800

    Paymongo key

--- services/PayMongo/CheckoutSession.php
+++ services/PayMongo/CheckoutSession.php
@@ -17,7 +17,7 @@ class CheckoutSession{
         'headers' => [
             'Content-Type' => 'application/json',
             'accept' => 'application/json',
-            'authorization' => "Basic {getenv('PAYMONGO_KEY')}"
+            'authorization' => "Basic ". getenv('PAYMONGO_KEY')
         ],
         ]);
 
