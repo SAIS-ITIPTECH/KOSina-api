cc7e7bb84e946fecf07d9926eebbf596cc0a0158
JadePulong <christianjadepulong6@gmail.com>
2026-05-07 21:25:10 +0800

    m

--- auth/LoginQuery.php
+++ auth/LoginQuery.php
@@ -6,7 +6,6 @@ class LoginQuery{
     private $execution;
 
     public function __construct(){
-        var_dump(getenv("DATABASE_HOSTNAME"), getenv("DATABASE_NAME"), getenv("DATABASE_USERNAME"));
         $database = new Database(getenv("DATABASE_HOSTNAME"), getenv("DATABASE_NAME"), getenv("DATABASE_USERNAME"));
         $this->pdo = $database->connectDatabase();
         $this->execution = new Execution;
