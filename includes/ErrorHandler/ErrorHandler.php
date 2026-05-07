ad2125ef4c09ff21708ea01057d541c81a75a7ec
JadePulong <christianjadepulong6@gmail.com>
2026-05-08 02:49:05 +0800

    FINALLYYYYYYYYY

--- includes/ErrorHandler/ErrorHandler.php
+++ includes/ErrorHandler/ErrorHandler.php
@@ -5,12 +5,12 @@ class ErrorHandler {
         http_response_code(500);
 
         if (getenv("APP_ENV") === 'development') {
-            error_log(json_encode([
+            echo json_encode([
                 "status" => "error",
                 "message" => $exception->getMessage(),
                 "line" => $exception->getLine(),
                 "file" => $exception->getFile()
-            ]));
+            ]);
         } else {
             
             echo json_encode([
