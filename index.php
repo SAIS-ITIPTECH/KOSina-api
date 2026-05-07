6372a5bad623c2a08aa70926595825a24979848f
JadePulong <christianjadepulong6@gmail.com>
2026-05-07 20:26:41 +0800

    yip

--- index.php
+++ index.php
@@ -66,8 +66,8 @@ class Main{
     private function setTarget(){
         $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
         $req = explode("/", trim($path, "/"));
-        $this->table = $req[1] ?? null;
-        $this->id = $req[2] ?? null;
+        $this->table = $req[0] ?? null;
+        $this->id = $req[1] ?? null;
         print_r(1);
         print_r(2);
     }
