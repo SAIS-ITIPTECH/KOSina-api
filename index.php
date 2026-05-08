89dd214f9401a05281f3ad01f5201222802b9635
JadePulong <christianjadepulong6@gmail.com>
2026-05-08 10:08:37 +0800

    cors update

--- index.php
+++ index.php
@@ -2,9 +2,18 @@
 
 //KakosaNiNina11
 // HEADERS
+$allowedDomain = [
+    "https://sais-itiptech.github.io/KOSina-Dashboard/",
+];
+
+if (!in_array($_SERVER['HTTP_ORIGIN'], $allowedDomain)) { 
+    http_response_code(401);
+    echo json_encode(["status" => "error", "message" => strtoupper("UNAUTHORIZED DOMAIN ORIGIN!")]);
+}
+
 header("Content-Type: application/json; charset=utf-8");
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+header("Access-Control-Allow-Methods: GET, POST, PATCH, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
 if ($_SERVER["REQUEST_METHOD"] === "OPTIONS") {
