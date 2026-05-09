2ddbbd44997e9177e6579b280d20d96e68fa6966
JadePulong <christianjadepulong6@gmail.com>
2026-05-09 15:44:33 +0800

    oml broo

--- index.php
+++ index.php
@@ -72,7 +72,7 @@ class Main{
         $req = explode("/", trim($path, "/"));
         $this->table = $req[0] ?? null;
         $this->id = $req[1] ?? null;
-        var_dump(gettype($id), $id)
+        var_dump(gettype($this->id), $this->id);
     }
 
     private function determine(){
