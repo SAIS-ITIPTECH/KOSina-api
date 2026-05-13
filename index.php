5f56995bf5b065297b7785ef0f353539dc40c6a0
JadePulong <christianjadepulong6@gmail.com>
2026-05-13 22:32:03 +0800

    token

--- index.php
+++ index.php
@@ -121,6 +121,7 @@ class Main{
     private function getDbCrendentials(){
         $tokenChecker = new TokenChecker();
         $token = $tokenChecker->decodeToken();
+        echo $token;
         $credentials = new CredentialsGraber($token);
         $credentials->connectCredentialsId();
         return ["dbName" => $credentials->getDbName(), "dbUsername" =>  $credentials->getDbUsername(), "dbPassword" =>  $credentials->getDbPassword()];
