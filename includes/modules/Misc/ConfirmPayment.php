241cb46e8a9f47007ed84b85c49cd4c521d1796b
JadePulong <christianjadepulong6@gmail.com>
2026-05-15 14:35:43 +0800

    echo clean up

--- includes/modules/Misc/ConfirmPayment.php
+++ includes/modules/Misc/ConfirmPayment.php
@@ -49,7 +49,6 @@ class ConfirmPayment{
         $stmt->bindValue(":setid", $this->id);
         $stmt->execute();
         $dailySaleId = $stmt->fetchColumn();
-        var_dump($dailySaleId, $this->id);
 
         $stmt = $this->pdo->prepare("
             UPDATE daily_sales
