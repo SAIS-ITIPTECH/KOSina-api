9790b59dbcc71fb62d0182cdb21bc8a1039f4976
JadePulong <christianjadepulong6@gmail.com>
2026-05-16 17:25:45 +0800

    1

--- includes/modules/History/HistoryModel.php
+++ includes/modules/History/HistoryModel.php
@@ -58,8 +58,7 @@ class HistoryModel {
     public function datePageValidator($dirtyDatePage){
         if($this->validator->checkEmpty("DATE PAGE", $dirtyDatePage) === null) { return false; }
         if($this->validator->checkSpecial("DATE PAGE", $dirtyDatePage, '/[^a-zA-Z0-9\-]/') === null) { return false; }
-        if($dirtyDatePage)
-        return true;
+        return false;
     }
 
     public function pageValidator($dirtyPage){
