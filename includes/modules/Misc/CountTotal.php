e2eea1b4cb28b1d128a083d2f9e7b4c5e2bb660c
JadePulong <christianjadepulong6@gmail.com>
2026-05-16 19:52:34 +0800

    echo cleanup

--- includes/modules/Misc/CountTotal.php
+++ includes/modules/Misc/CountTotal.php
@@ -21,26 +21,20 @@ class CountTotal{
     }
 
     public function count($datePage){
-        var_dump(1, $datePage);
         if (!$this->table) { return; }
         if (!$this->validateDatePage($datePage)) { return; }
 
-        var_dump(4, $datePage);
         $stmt = $this->pdo->prepare("SELECT COUNT(*) AS total FROM $this->table WHERE DATE(order_date) = DATE(:setDateLimit)");
         $stmt->bindValue(":setDateLimit", $datePage, PDO::PARAM_STR);
-        var_dump(5, $datePage);
         $this->execution->execute($stmt);
         $result = $this->execution->getResults();
-        var_dump(6, $datePage);
         echo json_encode($result[0]);
     }
 
     private function validateDatePage($datePage){
         $validation = new Validation();
-        var_dump(2, $datePage);
         if ($validation->checkEmpty("DATE PAGE", $datePage) === null) { return false; }
         if ($validation->checkSpecial("DATE PAGE", $datePage, '/[^a-zA-Z0-9\-]/') === null) { return false; }
-        var_dump(3, $datePage);
         return true;
     }
 }
