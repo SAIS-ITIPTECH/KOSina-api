919f0f93aae21e69470461c3442bfed53f2d5131
JadePulong <christianjadepulong6@gmail.com>
2026-05-16 16:49:42 +0800

    s

--- includes/Validation/Validation.php
+++ includes/Validation/Validation.php
@@ -22,7 +22,7 @@ class Validation{
     public function checkNumber($column, $value){
         echo $value;
         echo"running";
-        if(!isset($value) || filter_var($value, FILTER_VALIDATE_INT) === false){
+        if(filter_var($value, FILTER_VALIDATE_INT) === false){
             http_response_code(422);
             echo json_encode(["status" => "error", "message" => strtoupper("$column SHOULD BE A NUMBER!")]);
             return null;
