bfe1123e4f55fb0d7ddbfa6b25b3348327b985b9
JadePulong <christianjadepulong6@gmail.com>
2026-05-16 17:34:14 +0800

    1

--- includes/Validation/Validation.php
+++ includes/Validation/Validation.php
@@ -20,6 +20,7 @@ class Validation{
     }
 
     public function checkNumber($column, $value){
+        var_dump("1", $value);
         if(is_int($value)){
             http_response_code(422);
             echo json_encode(["status" => "error", "message" => strtoupper("$column SHOULD BE A NUMBER!")]);
