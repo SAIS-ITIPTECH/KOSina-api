d96114fdb7bc171ee5923f78eb6e92e8e1560d2a
JadePulong <christianjadepulong6@gmail.com>
2026-05-16 19:29:27 +0800

    1

--- index.php
+++ index.php
@@ -77,6 +77,7 @@ class Main{
         $req = explode("/", trim($path, "/"));
         $this->table = $req[0] ?? null;
         $this->id = $req[1] ?? null;
+        var_dump("0", $req[2]);
         $this->datePage = ($req[2] != "null") ? $req[2] : null;
         $this->page = ($req[3] != "null") ? (int) $req[3] : null;
     }
