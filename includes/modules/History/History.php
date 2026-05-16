2322ffb21653e157517f4f1626ebcfece87a00f0
JadePulong <christianjadepulong6@gmail.com>
2026-05-16 21:22:29 +0800

    1

--- includes/modules/History/History.php
+++ includes/modules/History/History.php
@@ -15,6 +15,7 @@ class History implements Controller {
     ){} 
 
     public function buildModel(){
+        var_dump("h", $this->datePage, $this->page);
         $model = new HistoryModel();
         $this->queryBuilder = new HistoryQueryBuilder($model, $this->pdo, $this->execution);
     }
