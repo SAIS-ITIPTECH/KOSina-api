44f2378f509e29fff782f982842c69b20a56e396
JadePulong <christianjadepulong6@gmail.com>
2026-05-16 18:15:05 +0800

    1

--- index.php
+++ index.php
@@ -79,6 +79,8 @@ class Main{
         $this->id = $req[1] ?? null;
         $this->datePage = ($req[2] != null) ? $req[2] : false;
         $this->page = ($req[3] != null) ? (int) $req[3] : false;
+        var_dump($req[2], $req[3]);
+        var_dump($this->datePage, $this->page);
     }
 
     private function determine(){
