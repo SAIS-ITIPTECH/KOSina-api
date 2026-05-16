159994e8f45211dd9578123911579e4fe2da5c50
JadePulong <christianjadepulong6@gmail.com>
2026-05-16 16:42:17 +0800

    4

--- includes/modules/History/HistoryModel.php
+++ includes/modules/History/HistoryModel.php
@@ -65,7 +65,7 @@ class HistoryModel {
         echo $dirtyPage;
         if($this->validator->checkEmpty("PAGE", $dirtyPage) === null) { return false; }
         echo "what";
-        if($this->validator->checkNumber("PAGE", $dirtyPage === null)) { return false; }
+        if($this->validator->checkNumber("PAGE", $dirtyPage) === null) { return false; }
         echo "tf";
         return true;
     }
