8243c2fd9231e1b071c4c09d3999afee3cc19d97
JadePulong <christianjadepulong6@gmail.com>
2026-05-20 01:21:02 +0800

    36

--- includes/modules/Products/ProductModel.php
+++ includes/modules/Products/ProductModel.php
@@ -19,6 +19,7 @@ class ProductModel {
     }
 
     public function validateFields(){
+        error_log(json_encode($this->userInput()));
         $name = $this->nameValidator();
         if (!$name) { return false; }
         $price = $this->priceValidator();
