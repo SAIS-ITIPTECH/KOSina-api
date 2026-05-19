aebbcbe2fb05a0cec2ef47066db79157fd29bbd9
JadePulong <christianjadepulong6@gmail.com>
2026-05-19 22:14:45 +0800

    role error

--- includes/modules/Categories/Categories.php
+++ includes/modules/Categories/Categories.php
@@ -49,7 +49,8 @@ class Categories implements Controller{
     }
     
     private function checkRole(){
-        error_log($this->role, gettype($this->role));
+        error_log($this->role);
+        error_log(gettype($this->role));
         if($this->role != "admin") {
             http_response_code(403);
             echo json_encode(["status" => "error", "message" => strtoupper("ONLY ADMIN CAN MODIFY KIOSK DATA!")]);
