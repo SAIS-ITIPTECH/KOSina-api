69f7da481f661fb212c051120c47d5f864133223
JadePulong <christianjadepulong6@gmail.com>
2026-05-21 16:33:24 +0800

    RAILWAY COMPATILBE ERROR HANDLING

--- includes/ErrorHandler/ErrorHandler.php
+++ includes/ErrorHandler/ErrorHandler.php
@@ -26,11 +26,18 @@ class ErrorHandler
         }
     }
 
-    private static function duplicate($exception) {
+    private static function duplicate(Throwable $exception) {
         $slicedMessage = explode('\'', $exception->getMessage());
         $duplicate = $slicedMessage[1] ?? "unknown";
-        $keyName = $slicedMessage[3] ?? "PRIMARY";
-        $column = ($keyName === "PRIMARY") ? "ID" : strtoupper(explode('.', str_replace('_', " ", $keyName))[1]);
+        $keyName   = $slicedMessage[3] ?? "PRIMARY";
+
+        if (str_contains($keyName, "PRIMARY")) {
+            $column = "ID";
+        } else {
+            $keyName = explode('.', $keyName);
+            $column  = strtoupper(str_replace('_', " ", end($keyName)));
+        }
+
         return "THE $column '$duplicate' ALREADY EXISTS";
     }
 }
