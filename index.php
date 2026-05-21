c8c1bd6a01339277ee95b9d27bef04a80d016c10
JadePulong <christianjadepulong6@gmail.com>
2026-05-21 11:34:18 +0800

    CORS FIX FR

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <?php
 
+// HEADERS
+header("Content-Type: application/json; charset=utf-8");
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PATCH, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+if ($_SERVER["REQUEST_METHOD"] === "OPTIONS") {
+    http_response_code(200);
+    exit;
+}
 
 // MODULES
 require_once __DIR__ . "/includes/modules/Categories/Categories.php";
