f6080d4893b84f3eb34b067ea12c91aae6a731bd
Christian Jade Pulong <christianjadepulong6@gmail.com>
2026-05-22 11:03:45 +0800

    sadadas

--- includes/modules/NewOrder/NewOrderModel.php
+++ includes/modules/NewOrder/NewOrderModel.php
@@ -131,7 +131,7 @@ class NewOrderModel
     private function fetchPriceFromDb($productId): float
     {
         $stmt = $this->pdo->prepare("SELECT price FROM product_list WHERE product_id = :setId");
-        $stmt->bindValue(":setId", $productId);
+        $stmt->bindValue(":setId", $productId, PDO::PARAM_STR);
         $stmt->execute();
         return (float) $stmt->fetchAll(PDO::FETCH_ASSOC)[0]["price"];
     }
